3ddef00b354ab593b494331095cff035c71998c0
kkmh1807 <kinekai.holthe@gmail.com>
2023-12-04 18:14:44 +0100

    Solve part 2 of 4th day

--- 4th/script.py
+++ 4th/script.py
@@ -1,6 +1,6 @@
 import math
 
-data = open("test.txt").read().split('\n')    
+data = open("input.txt").read().split('\n')    
 
 def p1(input):
     points = 0
@@ -20,4 +20,33 @@ def p1(input):
     
 # p1(data)
 
+def p2(input):
+    total_cards = 0
+    card_dict = {}
+    for line in input:
+        winning_numbers_found = 0
+        card = int(line.split(":")[0].split(" ")[-1])
+        
+        winning_numbers = line.split(": ")[1].split(" | ")[0].split()
+        card_numbers = line.split(": ")[1].split(" | ")[-1].split()
+
+        for number in card_numbers:
+            if number in winning_numbers:
+                winning_numbers_found += 1
+        
+        next_numbers = [i for i in range (card+1, card+winning_numbers_found+1)]
+        
+        if card not in card_dict:
+            card_dict[card] = (next_numbers, 1)
+    
+    for card, items in card_dict.items():
+        for i in range(len(items[0])):
+            card_dict[card + i + 1] = (card_dict[card + i + 1][0], card_dict[card + i + 1][1] + items[1])
+    
+    for card_amount in card_dict.values():
+        total_cards += card_amount[1]
+    
+    print(total_cards)
+
+p2(data)
         
